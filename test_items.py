e6d4ea0be6871154ef133763898d4e982f80e1bb
mr.melvin <korobov.mike@gmail.com>
2022-03-30 18:52:40 +0500

    Replace test_items.py

--- test_items.py
+++ test_items.py
@@ -1,24 +1,10 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
 
-def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
-                     help="Choose language")
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+def test_available_button_add_basket(browser):
+    browser.get(link)
+    time.sleep(3)
+    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket"), "Button not available!"
 
-@pytest.fixture(scope="function")
-def browser(request):
-    languages = ["ar","ca","cs","da","de","en-gb","el","es","fi","fr","it","ko","nl","pl","pt","pt-br","ro","ru","sk","uk","zh-hans"]
-    lang = request.config.getoption("language")
-    browser = None
-    if lang in languages:
-        print("\nstart chrome browser for test..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': lang})        
-        browser = webdriver.Chrome(options=options)
-    else:
-        raise pytest.UsageError("--language should be available")
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
